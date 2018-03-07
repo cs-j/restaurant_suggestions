620552158508d3ce2a912fa93a89704c63e6d1b3
brookescarlett <brookeyalof2017@u.northwestern.edu>
2018-03-07 15:29:01 -0500

    restaurants index and show pages

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,7 +1,10 @@
 class RestaurantsController < ApplicationController
+
   def index
+    @restaurants = Restaurant.search(params[:query])
   end
 
   def show
+    @restaurant = Restaurant.find(params[:id])
   end
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,4 +2,26 @@ class Restaurant < ApplicationRecord
   has_many :user_restaurants
   has_many :users, through: :user_restaurants
 
+  def self.search(query)
+    if query
+      Restaurant.where('restaurants.NAME LIKE ?', "%#{query}%")
+    else
+      self.all
+    end
+  end
+
+  def address_to_google_string
+    converted = self.address.split(" ").join("+").gsub(".", "")
+  end
+
+  def name_to_open_table_string
+    converted = self.name.split(" ").join("-").gsub("'", "").downcase
+  end
+
+  def date
+    t=Time.now
+    t.strftime "%Y-%m-%d"
+  end
+
+
 end
