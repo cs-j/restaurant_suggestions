9af0148e882ad6d2629fe5df47e32eb3456aae53
cs-j <cj@mail.co.de>
2018-03-08 18:45:25 -0500

    error messages, search page image

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,7 @@
 class ApplicationController < ActionController::Base
-  # helper_method :current_user, :logged_in?, :require_login
   protect_from_forgery with: :exception
   before_action :current_user
-  helper_method :require_logged_in, :current_user, :logged_in?
+  helper_method :current_user, :logged_in?
 
   def current_user
     @user = (User.find_by(id: session[:user_id]) || User.new)
--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,6 @@ Rails.application.routes.draw do
   get 'search', to: 'users#edit', as: :search
   get '/', to: 'users#index', as: :home
   get 'signup', to: 'users#new', as: :signup
+  post 'signup', to: 'users#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
