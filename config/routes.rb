5b8d95f05787440a96efa619e30ad1df3264c912
cs-j <cj@mail.co.de>
2018-03-08 13:17:54 -0500

    added home link functionality and changed route

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
   # resources :user_restaurants
   resources :restaurants, only: [:index, :show]
-  resources :users, except: [:edit]
+  resources :users, except: [:edit, :index]
   # resources :sessions
 
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   delete '/logout' => 'sessions#destroy'
   get 'search', to: 'users#edit', as: :search
+  get '/', to: 'users#index', as: :home
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
